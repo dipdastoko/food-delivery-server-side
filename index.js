31ac8a5dc5e28bff38c9c5510c04452d5b538e18
dipdastoko <dipdastoko@gmail.com>
2022-07-28 01:48:14 +0600

    .env file added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
 
@@ -8,6 +9,27 @@ app.use(cors());
 app.use(express.json());
 
 const port = process.env.PORT || 5000;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zqquk.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const database = client.db("food-delivery");
+        const foods = database.collection("foods");
+
+        // GET API
+        app.get('/foods', async (req, res) => {
+            const cursor = foods.find({});
+            const result = await cursor.toArray();
+            res.json(result);
+        })
+    }
+    finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('hello food delivery');
